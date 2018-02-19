bd45883c9babbca2f6a7ec177c3ff1ba8d0357b9
Kepler-22b <andriy@siteimage.com.ua>
2018-02-19 18:18:49 +0200

    Install blank theme

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+</head>
+<body>
