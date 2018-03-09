11d9305caf6fdc16dac8fcdf96ad4c4abaa8d50c
Kepler-22b <andriy@siteimage.com.ua>
2018-03-09 17:16:15 +0200

    Add webpack watcher through gulp

--- view/gulpfile.js
+++ view/gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const config = require('./webpack.config.js');
+config.watch = true;
 
 gulp.task('js', () => {
     gulp.src('src/app.js')
-        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(webpack(config))
         .pipe(gulp.dest('build/'))
 });
-
 
 gulp.task('watch', () => {
     gulp.watch('src/**/*.js', ['js']);
