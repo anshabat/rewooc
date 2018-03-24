768942b6d1fec15c2e303fc60410be9b00764dad
Kepler-22b <andriy@siteimage.com.ua>
2018-03-24 23:22:43 +0200

    Add PostCSS css-nested plugin

--- view/postcss.config.js
+++ view/postcss.config.js
@@ -3,7 +3,8 @@ module.exports = () => {
         plugins: {
             'autoprefixer':  {
                 browsers: ['last 4 versions', 'ie > 8', '> 1%']
-            }
+            },
+            'postcss-nested': {}
         }
     }
 };
