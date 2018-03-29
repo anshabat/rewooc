59193f320cdd91e13212ba0d0bf10c5ccad15505
Kepler-22b <andriy@siteimage.com.ua>
2018-03-29 22:27:31 +0300

    Add temp settings to Wordpress customizer

--- server/Customizer.php
+++ server/Customizer.php
@@ -4,35 +4,45 @@ class Customizer
 {
     public function __construct()
     {
-        add_action('customize_register', [$this, 'addCustomSettings']);
+        add_action("customize_register", [$this, "addCustomSettings"]);
     }
 
 
     public function addCustomSettings(\WP_Customize_Manager $wp_customize)
     {
-        $wp_customize->add_setting('font_color', [
-            'default' => '#000000'
+        $wp_customize->add_panel(get_template(), [
+            "title" => "Saleszone",
+            "priority" => 0,
         ]);
-        $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'font_color', [
-            'label' => __('Font color', get_template() . '_admin'),
-            'section' => 'title_tagline',
-            'settings' => 'font_color',
+
+        $wp_customize->add_section(get_template() . "_layout", [
+            "title" => "Temp",
+            "panel" => get_template(),
+            "description" => "Saleszone settings"
+        ]);
+
+        $wp_customize->add_setting(get_template() . "_font_color", [
+            "default" => "#000000"
+        ]);
+        $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, get_template() . "_font_color", [
+            "label" => __("Font color", get_template() . "_admin"),
+            "section" => get_template() . "_layout",
         ]));
 
 
-        $wp_customize->add_setting('show_banner', [
-            'default' => '1',
-            'sanitize_callback' => 'wc_string_to_bool'
+        $wp_customize->add_setting(get_template() . "_site_layout", [
+            "default" => "fluid"
         ]);
-        $wp_customize->add_control('show_banner', [
-            'type' => 'radio',
-            'label' => 'Show Banner',
-            'section' => 'title_tagline',
-            'choices' => array(
-                '1' => __('Yes', get_template() . '_admin'),
-                '0' => __('No', get_template() . '_admin')
+        $wp_customize->add_control(get_template() . "_site_layout", [
+            "type" => "radio",
+            "label" => "Site Layout",
+            "section" => get_template() . "_layout",
+            "choices" => array(
+                "fluid" => __("Full width", get_template() . "_admin"),
+                "boxed" => __("Boxed", get_template() . "_admin")
             ),
         ]);
     }
 }
+
 new Customizer();
