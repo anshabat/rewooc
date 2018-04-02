88ea7ce3615c7cb2c51a8ce26e9752fcc1f81d6c
BlackShabat <andriy@siteimage.com.ua>
2018-04-02 14:24:12 +0300

    Create SzNavigation mediator class and filter Navigation items data

---
+++ functions/SzNavigation.php
@@ -0,0 +1,30 @@
+<?php
+
+class SzNavigation {
+	private $position = null;
+
+	public function __construct( $position ) {
+		$this->position = $position;
+	}
+
+	public function getNav($allowedKeys = []) {
+		$navItems = wp_get_nav_menu_items( get_nav_menu_locations()[ $this->position ] );
+		//TODO доробити метод Filter Items (по publish)
+		$filteredItems = $this->filterItemsKeys( $navItems, $allowedKeys );
+		wp_localize_script( 'bundle', 'mainNavigation', $filteredItems );
+
+		return $filteredItems;
+	}
+
+	private function filterItemsKeys( $posts, $keys ) {
+		$result  = array_map( function ( $post ) use ( $keys ) {
+			$items = [];
+			foreach ($keys as $key){
+				$items[$key] = $post->$key;
+			}
+			return $items ? : $post;
+		}, $posts );
+
+		return $result;
+	}
+}
