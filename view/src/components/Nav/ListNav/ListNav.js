9516416a60fc296ffaeccbdd44d8d0156fdb1b3b
Kepler-22b <andriy@siteimage.com.ua>
2018-04-12 00:23:39 +0300

    Navigation component. Show and hide submenus on mouse hover

--- view/src/components/Nav/ListNav/ListNav.js
+++ view/src/components/Nav/ListNav/ListNav.js
@@ -6,16 +6,21 @@ const ListNav = (props) => {
     return (
         <ul className={`pc-list-nav pc-list-nav--depth-${props.depth}`}>
             {props.items.map(item => (
-                <li className="pc-list-nav__item" key={item.ID}>
-                    <a className="pc-list-nav__link"
-                       href={item.url}
-                    >{item.title}</a>
-                    <div className="pc-list-nav__drop">
-                        <Nav
-                            parentId={item.ID}
-                            depth={props.depth + 1}
-                        />
-                    </div>
+                <li className="pc-list-nav__item"
+                    onMouseEnter={() => props.showItem(item)}
+                    onMouseLeave={() => props.hideItem(item)}
+                    key={item.ID}
+                >
+                    <a className="pc-list-nav__link" href={item.url}>
+                        {item.title} {item.ID}
+                    </a>
+                    {props.openedItems.includes(item.ID) ?
+                        <div className="pc-list-nav__drop">
+                            <Nav
+                                parentId={item.ID}
+                                depth={props.depth + 1}
+                            />
+                        </div> : null}
                 </li>
             ))}
         </ul>
--- view/src/components/Nav/Nav.js
+++ view/src/components/Nav/Nav.js
@@ -2,33 +2,68 @@ import './Nav.css';
 import '../../vendor/css/shared/link.css';
 import React, {Component} from 'react';
 
-let items = [];
-let navs = [];
+let allItems = [];
+let allNavs = [];
 
-const Nav = (props) => {
+class Nav extends Component {
+    constructor(props) {
+        super(props);
+        /* Save origins in closure for reusing in component recursion */
+        allItems = props.items || allItems;
+        allNavs = props.navs || allNavs;
 
-    /* Save origins in closure for reusing in component recursion */
-    items = props.items || items;
-    navs = props.navs || navs;
+        this.childItems = allItems.filter(
+            item => Number(item.menu_item_parent) === props.parentId
+        );
+        this.ChildNav = allNavs[props.depth - 1] || allNavs[allNavs.length - 1];
+        this.state = {
+            openedItems: []
+        }
+    }
 
-    const childItems = items.filter(
-        item => Number(item.menu_item_parent) === props.parentId
-    );
+    hasChild(item) {
+        return allItems.some(i => Number(i.menu_item_parent) === item.ID)
+    }
 
-    const ChildNav = navs[props.depth - 1] || navs[navs.length - 1];
+    showItem(item) {
+        if (!this.hasChild(item)) {
+            return;
+        }
+        this.setState((prevState) => {
+            prevState.openedItems.push(item.ID);
+            return {openedItems: prevState.openedItems}
+        });
+    }
 
-    return (childItems.length) ? (
-        <ChildNav
-            items={childItems}
-            parentId={props.parentId}
-            depth={props.depth}
-        />
-    ) : null;
-};
+    hideItem(item) {
+        if (!this.hasChild(item)) {
+            return;
+        }
+        this.setState((prevState) => {
+            let index = prevState.openedItems.indexOf(item.ID);
+            prevState.openedItems.splice(index, 1);
+            return {openedItems: prevState.openedItems}
+        });
+    }
+
+    render() {
+        return (this.childItems.length && this.props.opened) ? (
+            <this.ChildNav
+                items={this.childItems}
+                parentId={this.props.parentId}
+                depth={this.props.depth}
+                showItem={this.showItem.bind(this)}
+                hideItem={this.hideItem.bind(this)}
+                openedItems={this.state.openedItems}
+            />
+        ) : null;
+    }
+}
 
 Nav.defaultProps = {
     parentId: 0,
-    depth: 1
+    depth: 1,
+    opened: true
 };
 
 export default Nav;
