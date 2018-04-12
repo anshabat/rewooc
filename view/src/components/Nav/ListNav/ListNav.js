e59bcf4b20b199be685ccc2edfd9e46350a83e27
Kepler-22b <andriy@siteimage.com.ua>
2018-04-12 22:08:18 +0300

    Navigation component. Add on hover toggle into Tee navigation

--- view/src/components/Nav/ListNav/ListNav.js
+++ view/src/components/Nav/ListNav/ListNav.js
@@ -2,29 +2,25 @@ import './ListNav.css';
 import React from 'react';
 import Nav from '../Nav';
 
-const ListNav = (props) => {
-    return (
-        <ul className={`pc-list-nav pc-list-nav--depth-${props.depth}`}>
-            {props.items.map(item => (
-                <li className="pc-list-nav__item"
-                    onMouseEnter={() => props.showItem(item)}
-                    onMouseLeave={() => props.hideItem(item)}
-                    key={item.ID}
-                >
-                    <a className="pc-list-nav__link" href={item.url}>
-                        {item.title} {item.ID}
-                    </a>
-                    {props.openedItems.includes(item.ID) ?
-                        <div className="pc-list-nav__drop">
-                            <Nav
-                                parentId={item.ID}
-                                depth={props.depth + 1}
-                            />
-                        </div> : null}
-                </li>
-            ))}
-        </ul>
-    )
-};
+const ListNav = (props) => (
+    <ul className={`pc-list-nav pc-list-nav--depth-${props.depth}`}>
+        {props.items.map(item => (
+            <li className="pc-list-nav__item"
+                onMouseEnter={() => props.showItem(item)}
+                onMouseLeave={() => props.hideItem(item)}
+                key={item.ID}
+            >
+                <a className="pc-list-nav__link" href={item.url}>
+                    {item.title}
+                </a>
+                {props.openedItems.includes(item.ID) ? (
+                    <div className="pc-list-nav__drop">
+                        <Nav parentId={item.ID} depth={props.depth + 1}/>
+                    </div>
+                ) : null}
+            </li>
+        ))}
+    </ul>
+);
 
 export default ListNav;
--- view/src/components/Nav/TreeNav/TreeNav.js
+++ view/src/components/Nav/TreeNav/TreeNav.js
@@ -3,17 +3,24 @@ import React from 'react';
 import Nav from '../Nav';
 
 const TreeNav = (props) => (
-    <ul className="pc-tree-nav">
+    <ul className={`pc-tree-nav pc-tree-nav--depth-${props.depth}`}>
         {props.items.map(item => (
-            <li className="pc-tree-nav__item" key={item.ID}>
-                <a className="pc-tree-nav__link"
-                   href={item.url}>{item.title}</a>
-                <div className="pc-tree-nav__drop">
-                    <Nav
-                        parentId={item.ID}
-                        depth={props.depth + 1}
-                    />
-                </div>
+            <li className="pc-tree-nav__item"
+                onMouseEnter={() => props.showItem(item)}
+                onMouseLeave={() => props.hideItem(item)}
+                key={item.ID}
+            >
+                <a className="pc-tree-nav__link" href={item.url}>
+                    {item.title}
+                </a>
+                {props.openedItems.includes(item.ID) ? (
+                    <div className="pc-tree-nav__drop">
+                        <Nav
+                            parentId={item.ID}
+                            depth={props.depth + 1}
+                        />
+                    </div>
+                ) : null}
             </li>
         ))}
     </ul>
