dcbe06ccd2245020fb2dcc365ecf8f63c964437f
Kepler-22b <andriy@siteimage.com.ua>
2018-04-17 22:16:51 +0300

    Mode App from container to components folder

--- view/src/index.js
+++ view/src/index.js
@@ -1,7 +1,7 @@
 import './vendor/css/core/reboot.css';
 import React from 'react';
 import ReactDOM from "react-dom";
-import App from './containers/App/App';
+import App from './components/App/App';
 
 ReactDOM.render(
     <App appData={window.salesZone} />,
