cf215d4a8e4674efc2e6da57b6bc4ff5f604fc6c
Kepler-22b <andriy@siteimage.com.ua>
2018-04-22 02:17:30 +0300

    Create Button component

--- view/src/components/SearchForm/SearchForm.js
+++ view/src/components/SearchForm/SearchForm.js
@@ -1,13 +1,13 @@
-import '../../assets/css/shared/btn.css';
 import './SearchForm.css';
 import React from 'react';
+import Button from '../UI/Button/Button';
 
 const SearchForm = (props) => {
     return (
         <div className="pc-search-form">
             <div className="pc-search-form__field">
                 <input className="pc-search-form__control" type="text" placeholder="Enter post title here..." />
-                <button className="pc-search-form__button ps-btn ps-btn--md ps-btn--secondary">Search</button>
+                <Button className="pc-search-form__button" size="md" color="secondary">Search</Button>
             </div>
             <div className="pc-search-form__autocomplete">
                 {/* <SearchFormResults/>*/}
