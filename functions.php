a11560ec2a2867adc152fe270e96b804b4e42fa3
Kepler-22b <andriy@siteimage.com.ua>
2018-05-16 00:40:07 +0300

    [Server] Small refactor

--- functions.php
+++ functions.php
@@ -1,28 +1,32 @@
 <?php
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
 }
 
-require_once(dirname(__FILE__) . "/server/Setup.php");
-require_once(dirname(__FILE__) . "/server/Customizer.php");
-require_once(dirname(__FILE__) . "/server/Options.php");
-require_once(dirname(__FILE__) . "/server/Navigation.php");
-require_once(dirname(__FILE__) . "/server/Products.php");
+require_once( dirname( __FILE__ ) . "/server/Setup.php" );
+require_once( dirname( __FILE__ ) . "/server/Customizer.php" );
+require_once( dirname( __FILE__ ) . "/server/Options.php" );
+require_once( dirname( __FILE__ ) . "/server/Navigation.php" );
+require_once( dirname( __FILE__ ) . "/server/Products.php" );
+require_once( dirname( __FILE__ ) . "/server/Api.php" );
 
 
-/**
- * Load header navigation
- */
-add_action('wp_enqueue_scripts', function () {
-    $headerNav = new Navigation('header_nav');
-    $headerNavItems = $headerNav->getNav(['ID', 'title', 'menu_item_parent', 'url']);
+add_action( 'init', function () {
+	$headerNav      = new Navigation( 'header_nav' );
+	$headerNavItems = $headerNav->getNav( [ 'ID', 'title', 'menu_item_parent', 'url' ] );
+	Api::addScriptData( 'headerNavItems', $headerNavItems );
 
-    $themeCustomizer = Customizer::getInstance();
-    $themeMods = $themeCustomizer->getMods();
+	$themeCustomizer = Customizer::getInstance();
+	$themeMods       = $themeCustomizer->getMods();
+	Api::addScriptData( 'themeMods', $themeMods );
 
-    wp_localize_script(get_template(), 'rewooc', [
-        'mainNavigation' => $headerNavItems,
-        'themeMods' => $themeMods
-    ]);
+	$featuredProducts = Products::getInstance()->getProducts( [
+		'featured' => true
+	] );
+	Api::addScriptData( 'featuredProducts', $featuredProducts );
+} );
 
-});
+
+add_action( 'wp_enqueue_scripts', function () {
+	Api::fetchScriptData();
+} );
