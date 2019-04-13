105bf61a7b3d14727b8ec6dac2de0d0068c32f9b
BlackShabat <andriy@siteimage.com.ua>
2019-04-13 12:51:10 +0300

    Fix Product category URL and gitignore file

--- pages/product-category.js
+++ pages/product-category.js
@@ -10,7 +10,7 @@ class ProductCategory extends Component {
     static async getInitialProps(context) {
         console.log(context.query);
         return new Promise((resolve) => {
-            axios.get(baseUrl('product-category/' + context.query.slug + '/')).then(({data}) => {
+            axios.get(baseUrl('/product-category/' + context.query.slug + '/')).then(({data}) => {
                 resolve({appData: data})
             });
         });
