7126732d2aab2c3f18bd54ef3cb3285cf924cb59
BlackShabat <andriy@siteimage.com.ua>
2020-04-07 10:40:02 +0300

    Install immutableJs and add Record to catalog reducer

--- src/reducers/catalog.js
+++ src/reducers/catalog.js
@@ -1,24 +1,30 @@
+import {Record} from 'immutable';
 import {CATALOG_PAGE_LOAD_FAIL, CATALOG_PAGE_LOAD_START, CATALOG_PAGE_LOAD_SUCCESS} from '../actions/loadCatalogPage';
 
-const initialState = {
+
+const initialState = Record({
   title: '',
   loading: true,
   error: false,
   products: []
-};
+});
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = new initialState(), action) => {
   const {type, error, payload} = action;
 
   switch (type) {
     case CATALOG_PAGE_LOAD_START:
-      return {...state, loading: true, error: false};
+      return state.set('loading', true).set('error', false);
     case CATALOG_PAGE_LOAD_SUCCESS:
-      return {...state, loading: false, error: false, products: payload.products, title: payload.title};
+      return state
+        .set('loading', false)
+        .set('error', false)
+        .set('products', payload.products)
+        .set('title', payload.title);
     case CATALOG_PAGE_LOAD_FAIL:
-      return {...state, loading: false, error: error};
+      return state.set('loading', false).set('error', error);
     default:
-      return {...state};
+      return state;
   }
 };
 export default reducer
