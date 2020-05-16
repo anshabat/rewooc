c6065d24688f98c42055bce7a4cb91e81e4a1be5
blackshabat <andriyshabat@gmail.com>
2020-05-16 19:29:55 +0300

    Set home config

--- server/wp/wp-content/themes/rewooc/app/Core/Setup.php
+++ server/wp/wp-content/themes/rewooc/app/Core/Setup.php
@@ -50,7 +50,7 @@ class Setup {
 		@header( 'Content-Type: application/json; charset=' . get_option( 'blog_charset' ) );
 		@header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
 		// Allow requests from other domains (CORS)
-		$origins[] = 'http://localhost:8080';
+		$origins[] = 'http://localhost:8081';
 
 		return $origins;
 	}
