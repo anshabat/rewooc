ccc47f2c9146be3f0b5edb9869607af052f3a399
blackshabat <andriyshabat@gmail.com>
2021-01-23 07:28:56 +0200

    use redux hooks to SignIn

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import "./Form.scss";
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import {signIn} from "../../redux/auth/authActions";
 import Content from "../../components/Layout/Content/Content";
 import FormField from "../../components/UI/Form/FormField/FormField";
@@ -8,84 +8,63 @@ import Button from "../../components/UI/Button/Button";
 import Message from "../../components/UI/Message/Message";
 import {selectAccountUser} from "../../redux/account/accountSelector";
 
-class SignIn extends Component {
+const SignIn = (props) => {
+  const {history} = props;
+  const loading = useSelector(state => state.auth.loading)
+  const error = useSelector(state => state.auth.error)
+  const user = useSelector(selectAccountUser)
+  const dispatch = useDispatch()
+  const loadingClass = loading ? "rw-form--is-loading" : "";
 
-  constructor(props) {
-    super(props);
-    this.submitHandler = this.submitHandler.bind(this);
-  }
-
-  componentDidMount() {
-    if (this.props.user) {
-      this.props.history.replace("/")
-    }
-  }
-
-  submitHandler = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
     const formElement = event.target;
     const formData = new FormData(formElement);
     const {username, password} = Object.fromEntries(formData.entries());
-    this.props.signInUser(username, password);
-  };
-
-
-  render() {
-    const {loading, error} = this.props;
-    const loadingClass = loading ? "rw-form--is-loading" : "";
-
-    return (
-      <Content title="Sign In" size="sm">
-        <form
-          className={`rw-form ${loadingClass}`.trim()}
-          action=""
-          id="sign-in"
-          onSubmit={this.submitHandler}
-        >
-          {error && (
-            <div className="rw-form__field">
-              <Message type="error">
-                {error.toString()}
-              </Message>
-            </div>
-          )}
-          <div className="rw-form__field">
-            <label htmlFor="sign-in-username" className="rw-form__label">Username or email</label>
-            <div className="rw-form__control">
-              <FormField name="username" id="sign-in-username" type="text"/>
-            </div>
-          </div>
-          <div className="rw-form__field">
-            <label htmlFor="sign-in-password" className="rw-form__label">Password</label>
-            <div className="rw-form__control">
-              <FormField name="password" id="sign-in-password" type="password"/>
-            </div>
-          </div>
-          <div className="rw-form__field">
-            <Button type="submit" size="lg" color="secondary">
-              Sign in
-            </Button>
-          </div>
-        </form>
-      </Content>
-    );
+    dispatch(signIn(username, password))
   }
+
+  useEffect(() => {
+    if (user) {
+      history.replace("/")
+    }
+  }, [user])
+
+  return (
+    <Content title="Sign In" size="sm">
+      <form
+        className={`rw-form ${loadingClass}`.trim()}
+        action=""
+        id="sign-in"
+        onSubmit={submitHandler}
+      >
+        {error && (
+          <div className="rw-form__field">
+            <Message type="error">
+              {error.toString()}
+            </Message>
+          </div>
+        )}
+        <div className="rw-form__field">
+          <label htmlFor="sign-in-username" className="rw-form__label">Username or email</label>
+          <div className="rw-form__control">
+            <FormField name="username" id="sign-in-username" type="text"/>
+          </div>
+        </div>
+        <div className="rw-form__field">
+          <label htmlFor="sign-in-password" className="rw-form__label">Password</label>
+          <div className="rw-form__control">
+            <FormField name="password" id="sign-in-password" type="password"/>
+          </div>
+        </div>
+        <div className="rw-form__field">
+          <Button type="submit" size="lg" color="secondary">
+            Sign in
+          </Button>
+        </div>
+      </form>
+    </Content>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    loading: state.auth.loading,
-    error: state.auth.error,
-    user: selectAccountUser(state)
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    signInUser: (username, password) => {
-      dispatch(signIn(username, password))
-    }
-  }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
+export default SignIn
