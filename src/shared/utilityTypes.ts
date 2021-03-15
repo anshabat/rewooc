834dd99a47387b406c60abbc6d152ec4f9e6d0cf
anshabat <andriyshabat@gmail.com>
2021-03-15 09:43:10 +0200

    remove immutable utility type and finally get rid of immutable.js

--- src/shared/utilityTypes.ts
+++ src/shared/utilityTypes.ts
@@ -1,12 +1,3 @@
-import { Map } from 'immutable'
-
-/**
- * Allow use object type T as Generic inside Map<T> type from immutable.js
- */
-export interface ImmutableMap<T> extends Map<string, any> {
-  get<K extends keyof T>(name: K): T[K]
-}
-
 /**
  * Eject T from Promise<T> type
  */
