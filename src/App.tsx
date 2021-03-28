7515dcde1c659c2785ce89393b6c658215f83106
anshabat <andriyshabat@gmail.com>
2021-03-28 18:07:08 +0300

    transform App to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { FC, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import { history } from './shared/history'
 import Root from './pages/Root'
@@ -10,43 +10,25 @@ import { AppStateType } from './redux/store'
 import { IAppState } from './redux/app/appTypes'
 import { selectApp } from './redux/app/appSelectors'
 
-interface StateProps {
-  app: IAppState
-}
-interface DispatchProps {
-  checkAuthAction: () => any
-}
-type Props = StateProps & DispatchProps
+const App: FC = () => {
+  const { data, loading } = useSelector<AppStateType, IAppState>(selectApp)
+  const dispatch = useDispatch()
 
-class App extends Component<Props> {
-  componentDidMount() {
-    const { checkAuthAction } = this.props
-    checkAuthAction()
-  }
+  useEffect(() => {
+    dispatch(checkAuth())
+  }, [])
 
-  render() {
-    const {
-      app: { data, loading },
-    } = this.props
+  if (loading || !data) return <PageLoader />
 
-    if (loading || !data) return <PageLoader />
+  //TODO add if (!data) - return Error page component instead of Loading state
 
-    //TODO add if (!data) - return Error page component instead of Loading state
-
-    return (
-      <AppProvider value={data}>
-        <ConnectedRouter history={history}>
-          <Root />
-        </ConnectedRouter>
-      </AppProvider>
-    )
-  }
+  return (
+    <AppProvider value={data}>
+      <ConnectedRouter history={history}>
+        <Root />
+      </ConnectedRouter>
+    </AppProvider>
+  )
 }
 
-const mapStateToProps = (state: AppStateType) => ({ app: selectApp(state) })
-const mapDispatchToProps = { checkAuthAction: checkAuth }
-
-export default connect<StateProps, DispatchProps, unknown, AppStateType>(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default App
