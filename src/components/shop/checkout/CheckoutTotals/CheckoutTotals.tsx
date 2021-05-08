b424890aa0765f256fbb6c7f63c0f41e2d4bf2df
anshabat <andriyshabat@gmail.com>
2021-05-08 09:50:36 +0300

    style checkout totals

--- src/components/shop/checkout/CheckoutTotals/CheckoutTotals.tsx
+++ src/components/shop/checkout/CheckoutTotals/CheckoutTotals.tsx
@@ -1,8 +1,10 @@
+import './CheckoutTotals.scss'
 import React, { FC } from 'react'
 import { useSelector } from 'react-redux'
 import {
   selectCartItems,
   selectCartTotalPrice,
+  selectCartTotalQuantity,
 } from '../../../../redux/cart/cartSelectors'
 import Price from '../../Price/Price'
 import { IDeliveryMethod } from 'app-data'
@@ -16,10 +18,14 @@ const CheckoutTotals: FC<IProps> = (props) => {
   const { total, delivery } = props
   const cartItems = useSelector(selectCartItems)
   const subtotal = useSelector(selectCartTotalPrice)
+  const count = useSelector(selectCartTotalQuantity)
 
   return (
     <div className="rw-cart-totals">
       <ul className="rw-cart-totals__products">
+        <h3 className="rw-cart-totals__products-title">
+          You have {count} products in the cart
+        </h3>
         {cartItems.map((item) => {
           return (
             <li key={item.key}>
@@ -28,26 +34,19 @@ const CheckoutTotals: FC<IProps> = (props) => {
           )
         })}
       </ul>
-      <br />
-      <br />
-      <dl className="rw-cart-totals__list">
+      <dl className="rw-cart-totals__fees">
         <dt>Subtotal</dt>
         <dd>
           <Price value={subtotal} />
         </dd>
-        {delivery && (
-          <>
-            <dt>Delivery</dt>
-            <dd>{delivery.cost}</dd>
-          </>
-        )}
+        <dt>Delivery</dt>
+        <dd>{delivery?.cost ?? 'Unknown'}</dd>
       </dl>
-      <br />
-      <br />
-
-      <dl className="rw-cart-totals__result">
+      <dl className="rw-cart-totals__fees">
         <dt>Total</dt>
-        <dd>{total ? <Price value={total} /> : '...'}</dd>
+        <dd>
+          <strong>{total ? <Price value={total} /> : '...'}</strong>
+        </dd>
       </dl>
     </div>
   )
