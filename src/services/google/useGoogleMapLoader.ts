61423dd275e431c5d49dfdacee3848f570f20d3d
anshabat <andriyshabat@gmail.com>
2021-08-24 13:31:09 +0300

    add google maps loader inside hook function

--- src/services/google/useGoogleMapLoader.ts
+++ src/services/google/useGoogleMapLoader.ts
@@ -2,16 +2,16 @@ import { Loader } from '@googlemaps/js-api-loader'
 import { Config } from '../../config'
 import { useEffect, useState } from 'react'
 
-const loader = new Loader({
-  apiKey: Config.googleMapApi,
-  libraries: ['places'],
-  language: 'en',
-})
-
 type GoogleType = typeof globalThis.google
 
 export function useGoogleMapLoader(): GoogleType | null {
   const [google, setGoogle] = useState<GoogleType | null>(null)
+
+  const loader = new Loader({
+    apiKey: Config.googleMapApi,
+    libraries: ['places'],
+    language: 'en',
+  })
 
   useEffect(() => {
     loader.load().then((google) => {
