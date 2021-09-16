6d31941eb1191af01e7a024a23c6830ee4ccea0c
anshabat <andriyshabat@gmail.com>
2021-09-16 17:15:06 +0300

    add dummy order filtering

--- src/components/shop/account/OrdersFilter/OrdersFilter.tsx
+++ src/components/shop/account/OrdersFilter/OrdersFilter.tsx
@@ -1,10 +1,13 @@
 import React, { FC, useEffect, useState } from 'react'
+import { IDeliveryMethod } from 'app-api'
 
 export interface OrderFilterAttributes {
   status: string[]
+  delivery: string[]
 }
 
 interface OrdersFilterProps {
+  deliveryValues: IDeliveryMethod[]
   onFilter: (attributes: OrderFilterAttributes) => void
 }
 
@@ -19,8 +22,9 @@ const statuses = [
 ]
 
 const OrdersFilter: FC<OrdersFilterProps> = (props) => {
-  const { onFilter } = props
+  const { onFilter, deliveryValues } = props
   const [activeStatuses, setActiveStatuses] = useState<string[]>([])
+  const [activeDelivery, setActiveDeliveries] = useState<string[]>([])
 
   const changeStatus = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const values = Array.from(e.target.options)
@@ -29,9 +33,16 @@ const OrdersFilter: FC<OrdersFilterProps> = (props) => {
     setActiveStatuses(values)
   }
 
+  const changeDelivery = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const values = Array.from(e.target.options)
+      .filter((option) => option.selected)
+      .map((option) => option.value)
+    setActiveDeliveries(values)
+  }
+
   useEffect(() => {
-    onFilter({ status: activeStatuses })
-  }, [activeStatuses.length])
+    onFilter({ status: activeStatuses, delivery: activeDelivery })
+  }, [activeStatuses.length, activeDelivery.length])
 
   return (
     <div className="rw-order-filter">
@@ -40,6 +51,15 @@ const OrdersFilter: FC<OrdersFilterProps> = (props) => {
           return (
             <option value={status} key={status}>
               {status}
+            </option>
+          )
+        })}
+      </select>
+      <select onChange={changeDelivery} multiple>
+        {deliveryValues.map((delivery) => {
+          return (
+            <option value={delivery.id} key={delivery.id}>
+              {delivery.title}
             </option>
           )
         })}
--- src/components/shop/account/OrdersList/OrdersList.tsx
+++ src/components/shop/account/OrdersList/OrdersList.tsx
@@ -5,6 +5,7 @@ import OrdersTable from '../OrdersTable/OrdersTable'
 import OrdersFilter, {
   OrderFilterAttributes,
 } from '../OrdersFilter/OrdersFilter'
+import { IDeliveryMethod } from 'app-api'
 
 interface OrdersListProps {
   orders: IOrder[]
@@ -15,18 +16,34 @@ const filterByStatus = (orders: IOrder[], statuses: string[]): IOrder[] => {
   return orders.filter((order) => statuses.includes(order.status))
 }
 
+const filterByDelivery = (orders: IOrder[], deliveries: string[]): IOrder[] => {
+  if (!deliveries.length) return orders
+  return orders.filter((order) =>
+    deliveries.includes(String(order.deliveryMethod.id))
+  )
+}
+
 const OrdersList: FC<OrdersListProps> = (props) => {
   const { orders } = props
   const [filteredOrders, setFilteredOrders] = useState(orders)
 
   const onFilterHandler = (attributes: OrderFilterAttributes) => {
     const newOrders = filterByStatus(orders, attributes.status)
-    setFilteredOrders(newOrders)
+    const newOrders2 = filterByDelivery(newOrders, attributes.delivery)
+    setFilteredOrders(newOrders2)
   }
+
+  const deliveryValues = orders.reduce<IDeliveryMethod[]>((prev, order) => {
+    const existing = prev.some((i) => i.id === order.deliveryMethod.id)
+    return existing ? prev : prev.concat(order.deliveryMethod)
+  }, [])
 
   return (
     <div className="rw-orders-list">
-      <OrdersFilter onFilter={onFilterHandler} />
+      <OrdersFilter
+        deliveryValues={deliveryValues}
+        onFilter={onFilterHandler}
+      />
       <OrdersTable orders={filteredOrders} />
     </div>
   )
