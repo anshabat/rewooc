90df3325a6341d465dc2c011f92ca52c517dda69
anshabat <andriyshabat@gmail.com>
2021-10-28 06:30:47 +0300

    update immer and axios libs

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -8,9 +8,8 @@ export const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token')
-    if (token) {
-      // eslint-disable-next-line no-param-reassign
-      config.headers.Authorization = localStorage.getItem('token')
+    if (token && config.headers) {
+      config.headers.Authorization = token
     }
     return config
   },
