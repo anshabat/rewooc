7bcab840f46d32f347f8e37c238f17160b07cb35
anshabat <andriy.shabat@ridewithvia.com>
2022-03-20 21:01:30 +0200

    install and comfigure trl-react plus rtl-jest-dom

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
 }
 
-export default config;
+export default config
