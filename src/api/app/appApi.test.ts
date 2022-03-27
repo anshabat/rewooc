10db3f1cb663066224a2da840020c647a0349606
anshabat <andriy.shabat@ridewithvia.com>
2022-03-27 12:03:10 +0300

    add nagative scenario for appApi.test.ts

--- src/api/app/appApi.test.ts
+++ src/api/app/appApi.test.ts
@@ -1,19 +1,30 @@
 import authApi from './appApi'
 import { instance } from '../instance'
+import { ErrorMessage } from '../../shared/errorMessages'
 
 jest.mock('../cart/cartHelpers', () => ({
   cartHashToItems: jest.fn(() => ({})),
 }))
 
 describe('fetchGeneralData', () => {
-  fit('should return data from backend', async () => {
+  it('should return data is response success', async () => {
     const mockGet = jest.spyOn(instance, 'get')
-    const mockData = { name: 'shop', cart: {} }
+    const mockData = { phone: '111', cart: {} }
     const responseData = { data: mockData }
 
     mockGet.mockResolvedValue(responseData)
     const result = await authApi.fetchGeneralData()
-
+    
     expect(result).toEqual(mockData)
   })
+
+  it('should throw an error if response fails', async () => {
+    const mockGet = jest.spyOn(instance, 'get')
+    mockGet.mockRejectedValue({ data: null })
+
+    const error = new Error(ErrorMessage.APP_FAIL_TO_FETCH_GENERAL_DATA)
+
+    await expect(authApi.fetchGeneralData()).rejects.toThrow(error);
+  })
+
 })
