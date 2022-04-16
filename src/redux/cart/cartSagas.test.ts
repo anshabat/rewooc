11ca143f0f2e4df2a4135d2ac62efa58d08308cb
anshabat <andriy.shabat@ridewithvia.com>
2022-04-16 19:51:01 +0300

    add unit test for addToCartSaga in cartSagas.ts

--- src/redux/cart/cartSagas.test.ts
+++ src/redux/cart/cartSagas.test.ts
@@ -14,6 +14,75 @@ import { addToCartSaga, deleteFromCartSaga } from './cartSagas'
 import { selectCartItems } from './cartSelectors'
 
 describe('cartSagas', () => {
+  describe('addToCartSaga', () => {
+    it('should add new product it is not in the cart', () => {
+      const productId = 1
+      const quantity = 1
+      const currentCartItems: ICartItem[] = []
+      const newCartItem = {
+        key: '1',
+        quantity: 1,
+        totalPrice: 100,
+        product: products[0],
+      }
+      const generator = addToCartSaga({
+        type: CART_ADD_PRODUCT,
+        payload: { productId, quantity },
+      })
+
+      const step1 = generator.next()
+      expect(step1.value).toEqual(select(selectCartItems))
+
+      const step2 = generator.next(currentCartItems as any)
+      expect(step2.value).toEqual(call(cartApi.addToCart, productId, quantity))
+
+      const step3 = generator.next(newCartItem as any)
+      expect(step3.value).toEqual(put(addToCartSuccess(newCartItem)))
+    })
+    
+    it('should increment quantity if product is already in the cart', () => {
+      const productId = 1
+      const quantity = 1
+      const cartItems: ICartItem[] = [
+        {
+          key: '1',
+          quantity: 1,
+          totalPrice: 100,
+          product: products[0],
+        },
+      ]
+      const generator = addToCartSaga({
+        type: CART_ADD_PRODUCT,
+        payload: { productId, quantity },
+      })
+
+      const step1 = generator.next()
+      expect(step1.value).toEqual(select(selectCartItems))
+
+      const step2 = generator.next(cartItems as any)
+      expect(step2.value).toEqual(put(setCartProductQuantity('1', 2)))
+    })
+
+    it('should throw an error if cartApi.addToCart fail', () => {
+      const productId = 1
+      const quantity = 1
+      const error = new Error()
+      const generator = addToCartSaga({
+        type: CART_ADD_PRODUCT,
+        payload: { productId, quantity },
+      })
+
+      const step1 = generator.next()
+      expect(step1.value).toEqual(select(selectCartItems))
+
+      const step2 = generator.next([] as any)
+      expect(step2.value).toEqual(call(cartApi.addToCart, productId, quantity))
+
+      generator.next()
+      const errorStep = generator.throw(error)
+      expect(errorStep.value).toEqual(put(addToCartFail(error)))
+    })
+  })
   describe('deleteFromCartSaga', () => {
     it('should succeed', () => {
       const productKey = '1'
