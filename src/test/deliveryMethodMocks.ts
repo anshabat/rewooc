a0582a382e0abbecfbd36c598a8792024839090b
anshabat <andriy.shabat@ridewithvia.com>
2022-08-05 12:47:00 +0300

    Test(mocks): create Delivery Methods Mock function

---
+++ src/test/deliveryMethodMocks.ts
@@ -0,0 +1,29 @@
+import { IDeliveryMethod } from 'api'
+
+export const orders: IDeliveryMethod[] = [
+  {
+    id: 'id_1',
+    title: 'Delivery_1',
+    cost: 0,
+    order: 1,
+    enabled: 'yes',
+  },
+  {
+    id: 'id_2',
+    title: 'Delivery_2',
+    cost: 10,
+    order: 1,
+    enabled: 'yes',
+  },
+  {
+    id: 'id_3',
+    title: 'Delivery_3',
+    cost: 100,
+    order: 1,
+    enabled: 'yes',
+  },
+]
+
+export function getDeliveryMethodMock(): IDeliveryMethod[] {
+  return [...orders]
+}
